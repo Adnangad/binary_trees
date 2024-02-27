91914a7df887f7b363b51d081ddbf3a6ec14436f
Adnangad <gardobuyaadnan@gmail.com>
2024-02-27 06:51:32 -0800

    trying to fix 1 and 2

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - measeures depth of binary tree
+ * @tree: pointer to the root nod of the tree
+ *
+ * Return:height of the tree or 0.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+        size_t left, right;
+
+        if (tree == NULL)
+                return (0);
+        left = binary_tree_depth(tree->left);
+        right = binary_tree_depth(tree->right);
+
+        if (left > right)
+                return (left + 1);
+        else
+                return (right + 1);
+}
