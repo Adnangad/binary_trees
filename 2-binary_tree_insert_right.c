7abe0542652e49eeba0444e5a66b89f86c16b435
Adnangad <gardobuyaadnan@gmail.com>
2024-02-27 06:54:57 -0800

    trying to fix 1 and 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,12 +11,18 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new;
 
-	if (new == NULL || parent == NULL)
+	if (parent == NULL)
 	{
 		return (NULL);
 	}
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+        {
+                return (NULL);
+        }
 	new->parent = parent;
 	new->n = value;
 	new->right = NULL;
