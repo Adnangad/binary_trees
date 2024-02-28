8fc0f671b39166892a7397daa7dfac58b1fe1c6a
Adnangad <gardobuyaadnan@gmail.com>
2024-02-28 04:28:09 -0800

    15-full, 14-balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,25 +3,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factory oof a binary tree
+ * binary_tree_height - measures the balance factory oof a binary tree
  * @tree:pointer to the root node of the tree
  *
  * Return: the balance or 0.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t l;
-        size_t r;
+	size_t l;
+	size_t r;
 
-        if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-                return (0);
-        l = binary_tree_height(tree->left) + 1;
-        r = binary_tree_height(tree->right) + 1;
-        if (l > r)
-                return (l);
-        else
-                return (r);
+	if (!tree)
+		return (0);
+	l = binary_tree_height(tree->left);
+	r = binary_tree_height(tree->right);
+
+	if (r >= l)
+		return (r + 1);
+	else
+		return (l + 1);
 }
+
+/**
+ * binary_tree_balance - measures the balance factory oof a binary tree
+ * @tree:pointer to the root node of the tree
+ *
+ * Return: the balance or 0.
+ */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int l, r;
